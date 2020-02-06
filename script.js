a3a4a187d5803a4164dd229d2bd46ee65ebbcd89
Riccardo van der Horst <riccardo271002@gmail.com>
2020-02-06 15:05:09 +0100

    uitwerking

---
+++ script.js
@@ -0,0 +1,16 @@
+var obj = {
+    "Naam": "Zara",
+    "Score": 36,
+    "Begin datum": "21 Januari 2016",
+    "Vrienden": [
+        "Mika",
+        "Josef",
+        "Maria",
+        "Sumail",
+        "Lotte"
+    ],
+    "Gekwalificeerd": true
+
+};
+
+console.log(JSON.stringify(obj));
